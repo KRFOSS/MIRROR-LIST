3ae9539d6caa3fd06e5408f961ea4905bab0d429
imnyang <me@imnya.ng>
2025-07-23 22:44:46 +0900

    6210, 6211

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,7 +4,7 @@ import mirrorlist from "./routes/mirrorlist";
 const app = new Elysia({ prefix: "/api" })
   .get("/", () => Date.now())
   .use(mirrorlist)
-  .listen(3000);
+  .listen(6210);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
